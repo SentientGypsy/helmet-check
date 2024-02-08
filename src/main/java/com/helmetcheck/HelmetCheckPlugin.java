ddcba4b2ec00f5cbd194ac02ac0c8a04b958a3c4
Sentientgypsy <25094690+SentientGypsy@users.noreply.github.com>
2024-02-08 08:32:42 -0600

    trying to figure this out

--- src/main/java/com/helmetcheck/HelmetCheckPlugin.java
+++ src/main/java/com/helmetcheck/HelmetCheckPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.helmetcheck;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,26 +14,26 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Helmet Check"
 )
-public class ExamplePlugin extends Plugin
+public class HelmetCheckPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private HelmetCheckConfig config;
 
 	@Override
 	protected void startUp() throws Exception
 	{
-		log.info("Example started!");
+		log.info("Helmet check has started!");
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.info("Example stopped!");
+		log.info("Helmet check has stopped!");
 	}
 
 	@Subscribe
@@ -41,13 +41,13 @@ public class ExamplePlugin extends Plugin
 	{
 		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Helmet-Check Plugin says: " + config.greeting(), null);
 		}
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	HelmetCheckConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(HelmetCheckConfig.class);
 	}
 }
